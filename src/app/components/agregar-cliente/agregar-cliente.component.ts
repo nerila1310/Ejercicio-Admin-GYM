7d9df0540376008f5f7877d2a5adb77b0bded6e5
Neri Laredo <nerila1310@gmail.com>
2021-01-25 11:54:58 -0600

    Validacion de formularios Agregar clientes

--- src/app/components/agregar-cliente/agregar-cliente.component.ts
+++ src/app/components/agregar-cliente/agregar-cliente.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-agregar-cliente',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./agregar-cliente.component.scss']
 })
 export class AgregarClienteComponent implements OnInit {
-
-  constructor() { }
+  
+  formularioCliente!: FormGroup;
+  
+  constructor( private fb: FormBuilder) { }
 
   ngOnInit(): void {
+
+    this.formularioCliente  = this.fb.group({
+      nombre:['', Validators.required],
+      apellido:['', Validators.required],
+      correo:['', Validators.compose([
+        Validators.required,
+        Validators.email
+      ])],
+      curp:[''],
+      fechaNacimiento:['', Validators.required],
+      telefono:[''],
+      imgUrl: ['', Validators.required]
+    })
+  }
+
+  agregar(){
+    console.log(this.formularioCliente.value);
   }
 
 }
